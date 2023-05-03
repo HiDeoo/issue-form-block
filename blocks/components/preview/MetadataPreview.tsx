d26c091cf0b5670b2ae082c50c9c78e54d242f4f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-05-03 18:10:27 +0200

    feat: metadata preview separator

--- blocks/components/preview/MetadataPreview.tsx
+++ blocks/components/preview/MetadataPreview.tsx
@@ -7,29 +7,40 @@ export function MetadataPreview() {
   const metadata = useAtomValue(issueFormMetadataAtom)
 
   return (
-    <Box
-      sx={{
-        alignItems: 'center',
-        bg: 'canvas.inset',
-        border: 1,
-        borderColor: 'border.default',
-        borderRadius: 2,
-        borderStyle: 'solid',
-        display: 'flex',
-        gap: 3,
-        justifyContent: 'space-between',
-        p: 2,
-      }}
-    >
-      <Box overflow="hidden">
-        <Heading sx={{ fontSize: 2, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-          {metadata.name}
-        </Heading>
-        <Text color="fg.muted" fontSize={1}>
-          {metadata.description}
-        </Text>
+    <>
+      <Box
+        sx={{
+          alignItems: 'center',
+          bg: 'canvas.inset',
+          border: 1,
+          borderColor: 'border.default',
+          borderRadius: 2,
+          borderStyle: 'solid',
+          display: 'flex',
+          gap: 3,
+          justifyContent: 'space-between',
+          p: 2,
+        }}
+      >
+        <Box overflow="hidden">
+          <Heading sx={{ fontSize: 2, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+            {metadata.name}
+          </Heading>
+          <Text color="fg.muted" fontSize={1}>
+            {metadata.description}
+          </Text>
+        </Box>
+        <Button variant="primary">Get started</Button>
       </Box>
-      <Button variant="primary">Get started</Button>
-    </Box>
+      <Box
+        sx={{
+          border: 0,
+          borderTop: 1,
+          borderColor: 'border.default',
+          borderStyle: 'dashed',
+          mt: 1,
+        }}
+      />
+    </>
   )
 }
